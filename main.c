d9604bcc17546a8f09c611ecdf81e1b125ad09d8
Yeonji <liyeonji@gmail.com>
2016-10-21 19:35:55 +0800

    Add License

--- main.c
+++ main.c
@@ -1,3 +1,27 @@
+/********************************************************************************
+ * The MIT License
+ *
+ * Copyright (c) 2016 shydyl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+
 #ifndef F_CPU
 #define F_CPU 16000000UL
 #endif
@@ -8,11 +32,12 @@
 
 volatile unsigned int time=0;
 volatile unsigned int i=0;
+
 int main( void )
 {
-	
 	extern volatile unsigned int time;
 	int beep_flag=0;
+	
 	DDRB=0x23;
 	DDRC=0xFF;
 	PORTB=0x00;
